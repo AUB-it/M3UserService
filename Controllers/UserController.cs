aeeacdb7c127d109f7c9312bfac215942ee3ad5b
Umutnaz <umutnaz@outlook.dk>
2026-02-27 11:53:01 +0100

    Initial commit - UserService

---
+++ Controllers/UserController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace UserService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase
+{
+
+    private static List<User> _users = new List<User>()
+    {
+        new()
+        {
+            Id = new Guid("c9fcbc4b-d2d1-4664-9079-dae78a1de446"),
+            Name = "JoMoney",
+            Address1 = "123 Main Street",
+            City = "London",
+            PostalCode = 420,
+            EmailAddress = "JoMoney@gmail.com",
+            PhoneNumber = "88888888"
+            
+        }
+    };
+
+    private readonly ILogger<UserController> _logger;
+
+    public UserController(ILogger<UserController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("userId")]
+    public User Get(Guid userId)
+    {
+        return _users.Where(u => u.Id == userId).First();
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    // 1. Lav OpenAPI endepunkt (default: /openapi/v1.json)
+    app.MapOpenApi();
+    
+    
+    // 2. Start Scalar webUi /default: /scalar/v1)
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
